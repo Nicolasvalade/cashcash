5b9a7e2f4ae15f6a05398b8eb859f44dd57a943a
Nicolasvalade <nicolas@valade.mobi>
2022-02-22 23:28:09 +0100

    Ajout filtre techniciens de l'agence uniquement

--- controllers/interv_details.php
+++ controllers/interv_details.php
@@ -1,22 +1,12 @@
 <?php
 include_once 'models/interv.php';
 include_once 'models/tech_arrays.php';
-if(isset($_POST['affecter_a']))
-{
-    $success = affecter_a($_GET['id'],$_POST['affecter_a']);
-    if(!$success)
-    {
+if (isset($_POST['affecter_a'])) {
+    $success = affecter_a($_GET['id'], $_POST['affecter_a']);
+    if (!$success) {
         die("Erreur.");
     }
-    else
-    {
-        include 'controllers/pdf/interv_pdf.php';
-    }
 }
-else
-{
-    $interv = get_intervention_by_id($_GET['id']);
-    $all_tech = get_all_techniciens();
-    include 'views/interv_details.php';
-}
-
+$interv = get_intervention_by_id($_GET['id']);
+$all_tech = get_techniciens_agence($interv['code_agence'], 'nom');
+include 'views/interv_details.php';
--- views/interv_details.php
+++ views/interv_details.php
@@ -6,26 +6,22 @@ ob_start();
 <p>Client : <?= $interv['client'] ?></p>
 <p>Planifiée le : <?= $interv['date_heure'] ?></p>
 <p>Etat : <?= $interv['e_libelle'] ?></p>
-<?php if($interv['t_nom'] !== NULL) : ?>
-    <p>Affectée à : <?= $interv['t_prenom'] . ' ' . $interv['t_nom'] ?></p>
-    <a href="<?= $index ?>/pdf/intervention?id=<?= $interv['id'] ?>">
-        <button>Fiche PDF</button>
-    </a>
-<?php else : ?>
+<?php if ($interv['e_id'] == 1) : ?>
     <form method="POST" action="#">
         <select name="affecter_a">
-            <?php foreach($all_tech as $tech) : ?>
-                <option value="<?=$tech['matricule'] ?>"><?="$tech[nom] $tech[prenom]"?></option>
+            <?php foreach ($all_tech as $tech) : ?>
+                <option value="<?= $tech['matricule'] ?>"><?= "$tech[nom] $tech[prenom]" ?></option>
             <?php endforeach; ?>
         </select>
         <button type="submit">Affecter</button>
     </form>
+<?php elseif ($interv['e_id'] != 4) : ?>
+    <p>Affectée à : <?= $interv['t_prenom'] . ' ' . $interv['t_nom'] ?></p>
+    <a href="<?= $index ?>/pdf/intervention?id=<?= $interv['id'] ?>">
+        <button>Fiche PDF</button>
+    </a>
 <?php endif; ?>
 <?php
 // mettre tout le html écris au-dessus dans la variable $content au lieu de l'afficher
 $content = ob_get_clean();
-require_once 'base_template.php'
-?>
-<script>
-    // 
-</script>
+require_once 'templates/base.php';
