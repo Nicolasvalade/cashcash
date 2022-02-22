74a7bfd34ef3a8b29b96300ca8fb708330071988
Nicolasvalade <nicolas@valade.mobi>
2022-02-23 00:50:25 +0100

    lang fr

--- templates/base.php
+++ templates/base.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <head>
-        <link href="<?= $domain ?>/styles/base.css" rel="stylesheet">
-        <title><?= $title ?></title>
-    </head>
+    <link href="<?= $domaine ?>/styles/base.css" rel="stylesheet">
+    <title><?= $title ?></title>
+</head>
 
 <body>
     <nav>
