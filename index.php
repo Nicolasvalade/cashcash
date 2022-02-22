7401818d0d70f75e799b77f001fa97326bd00b7f
Nicolasvalade <nicolas@valade.mobi>
2022-02-22 18:13:58 +0100

    Route vers PDF

--- index.php
+++ index.php
@@ -8,7 +8,11 @@ if ($index == $uri) {
   require_once 'controllers/interv_liste.php';
 } elseif ($index . '/intervention' == $uri && isset($_GET['id'])) {
   require_once 'controllers/interv_details.php';
-} else {
+} elseif ($index . '/pdf/intervention' == $uri && isset($_GET['id'])) {
+  require_once 'controllers/pdf/interv_pdf.php';
+}  elseif ($index . '/pdf/test' == $uri) {
+  require_once 'controllers/pdf/test.php';
+}  else {
   header('Status: 404 Not Found');
   echo '<!DOCTYPE html><html><body><h1>' . $uri . ' : page Not Found</h1></body></html>';
 }
