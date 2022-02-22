47b8348171a885a2e027f7eb13335ad23815ae11
Nicolasvalade <nicolas@valade.mobi>
2022-02-22 18:16:49 +0100

    Interventions par état

--- views/interv_liste.php
+++ views/interv_liste.php
@@ -1,17 +1,35 @@
 <?php
-$title = 'Liste des intervs';
+$title = 'Liste des interventions';
 ob_start();
 ?>
 <h1>Liste des interventions</h1>
+
+<?php if(count($interv_a_affecter)===0) : ?>
+  <p>Aucune intervention à affecter.</p>
+<?php else : ?>
+  <p>Interventions à affecter :</p>
+  <ul>
+    <?php foreach ($interv_a_affecter as $interv) : ?>
+      <li>
+        <a href="<?= $index ?>/intervention?id=<?= $interv['id'] ?>">
+          <?= "Intervention $interv[id] du $interv[date_heure], $interv[client]" ?>
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+<?php endif; ?>
+
+<p>Toutes les interventions</p>
 <ul>
-  <?php foreach ($interv_array as $interv) : ?>
+  <?php foreach ($all_interv as $interv) : ?>
     <li>
       <a href="<?= $index ?>/intervention?id=<?= $interv['id'] ?>">
-        <?= "Intervention $interv[id], $interv[client] : $interv[t_prenom] $interv[t_nom]" ?>
+        <?= "Intervention $interv[id] du $interv[date_heure], $interv[client]" ?>
       </a>
     </li>
   <?php endforeach; ?>
 </ul>
+
 <?php
 // mettre tout le html écris au-dessus dans la variable $content au lieu de l'afficher
 $content = ob_get_clean();
