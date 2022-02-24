6c96509f36e26d84361c9bdc87ac884767d9dc42
Nicolasvalade <nicolas@valade.mobi>
2022-02-24 01:22:17 +0100

    requête update

--- models/interv.php
+++ models/interv.php
@@ -31,10 +31,31 @@ function affecter_a($id_intervention, $matricule)
 
   // préparer la requête et l'exécuter
   $sql = "UPDATE intervention
-    SET matricule=:matricule, etat=2
+    SET matricule=:matricule, etat=2, date_heure=date_heure
     WHERE id=:id AND etat=1";
   $stmt = $conn->prepare($sql);
   $stmt->bindParam(':id', $id_intervention, PDO::PARAM_INT);
+  $stmt->bindParam(':matricule', $matricule, PDO::PARAM_STR);
+  $result = $stmt->execute();
+
+  // fermer la connexion
+  $conn = null;
+  return $result;
+}
+
+function update_intervention($id_intervention, $date_heure, $matricule)
+{
+  include_once 'db_config.php';
+  // se connecter
+  $conn = getConnexion();
+
+  // préparer la requête et l'exécuter
+  $sql = "UPDATE intervention
+    SET date_heure=:date_heure, matricule=:matricule
+    WHERE id=:id";
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(':id', $id_intervention, PDO::PARAM_INT);
+  $stmt->bindParam(':date_heure', $date_heure, PDO::PARAM_STR);
   $stmt->bindParam(':matricule', $matricule, PDO::PARAM_STR);
   $result = $stmt->execute();
 
