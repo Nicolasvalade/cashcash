cd8037378deb97d3d37fc43392541eaf30b90a82
Nicolasvalade <nicolas@valade.mobi>
2022-02-26 16:37:56 +0100

    valider une intervention

--- controllers/tech/interv_valid.php
+++ controllers/tech/interv_valid.php
@@ -1,34 +1,29 @@
 <?php
 include_once 'models/interv.php';
-include_once 'models/tech_arrays.php';
+include_once 'models/mat_arrays.php';
 include_once 'util/dates.php';
 include_once 'util/erreurs.php';
 
-if (isset($_POST['valider'])) {
-    $materiels=[];
-    $i = 0;
-    while (isset($_POST["n_serie_$i"])&&isset($_POST["comm_$i"])&&isset($_POST["temps_$i"])){
-        $materiels[]= [
-            "n_serie" => $_POST["n_serie_$i"],
-            "commentaire" => $_POST["comm_$i"],
-            "temps_passe" => $_POST["temps_$i"]
-        ];
-    }
-    if (count($materiels)==0){
-        header("Location: $uri?id=$_GET[id]&erreur=tiv1");
-        die();
-    }
-    $success = valider_interv($_GET['id'], $materiels);
-    if (!$success) {
-        header("Location: $uri?id=$_GET[id]&erreur=tiv2");
-        die();
-    }
-    header("Location: $uri?id=$_GET[id]");
+if (isset($_POST['materiels'])) {
+
+  $materiels = $_POST['materiels'];
+
+  if (count($materiels) == 0) {
+    header("Location: $uri?id=$_GET[id]&erreur=tiv1");
+    die();
+  }
+  $success = valider_interv($_GET['id'], $materiels);
+  if (!$success) {
+    header("Location: $uri?id=$_GET[id]&erreur=tiv2");
+    die();
+  }
+  header("Location: $uri?id=$_GET[id]");
 }
 
 // récupérer le code erreur pour trouver le message à afficher
 $code_erreur = isset($_GET['erreur']) ? $_GET['erreur'] : "";
 $erreur = get_msg_erreur($code_erreur);
 
+$all_mat = get_materiels_by_interv($_GET['id']);
 $interv = get_intervention_by_id($_GET['id']);
-include 'views/admin/interv_valid.php';
+include 'views/tech/interv_valid.php';
